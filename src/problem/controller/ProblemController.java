9040153139eed9574bbbea0cf4f3716c49bb3bb1
MarkGrayson <mpal7487@740s-pl-193.local>
2016-09-29 12:24:19 -0600

    Has catch and try

--- src/problem/controller/ProblemController.java
+++ src/problem/controller/ProblemController.java
@@ -19,9 +19,31 @@ public class ProblemController
 		{
 			display.displayMessage("Then Suddenly Words!");
 		
-			answer = display.collectResponse("Are you tired?!");
-		
-			count++;
+			answer = display.collectResponse("Are you tired?!");			
 		}
+	}
+		/**
+		 * Checks a supplied String value to see if it can be converted to a double.
+		 * @param potentialValue The supplied String.
+		 * @return Whether the conversion to a double is possible as a boolea value.	
+		 */
+		
+		private boolean isDouble(String potentialValue)
+		{
+			boolean validDouble = false
+					
+			try
+			{
+				double testValue = Double.parseDouble(potentialValue);
+				validDouble = true;
+			}
+			catch)NumberFOrmatException notDoubleError)
+			{
+				display.displayMessage("That was not a double =:<");
+			}
+		
+		
+		return validDouble
+		
 	}
 }
